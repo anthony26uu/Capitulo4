3f0ee2246e620b87a419ae815f2a07e499e39f96
Anthony Javier Santana Polanco <anthony26uu@gmail.com>
2017-01-13 00:59:02 -0400

    ModfEj1

---
+++ Ej1/Ej1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int resultado = 0;
+
+            Console.WriteLine("Ingrese Numero a multiplicar");
+            n = int.Parse(Console.ReadLine());
+            for(int i=1; i<=10;i++)
+            {
+                resultado = n * i;
+                Console.WriteLine(n + "x" + i + "=" + resultado);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
