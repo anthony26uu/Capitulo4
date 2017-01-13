83ebec9d70673d59de1695d4af805508f8be828c
Anthony Javier Santana Polanco <anthony26uu@gmail.com>
2017-01-13 17:58:35 -0400

    ModificacionEj5

--- Ej5/Ej5/Program.cs
+++ Ej5/Ej5/Program.cs
@@ -16,41 +16,29 @@ namespace Ej5
         static void Main()
         {
             int num;
-
-
-          
-
-
             Console.WriteLine("Ingrese Numero de edades");
             num = int.Parse(Console.ReadLine());
             int s=0;
             int[] edades = new int[num];
 
             
-
             for (int i = 0; i < edades.Length; i++)
             {
                 Console.WriteLine("Ingrese Edad ");
                 edades[i] = int.Parse(Console.ReadLine());
-
-               
+ 
             }
             
             for (int id=0; id<edades.Length; id++)
             {
-                Console.WriteLine("Elemento {0}: {1}", id, edades[id]);
-
-               
+                Console.WriteLine("Elemento {0}: {1}", id, edades[id])
                     s += edades[id];
-              
-
-            }
+             }
 
             Console.WriteLine("Su suma es "+s);
             Console.WriteLine("Promedio de edades es " + s / edades.Length);
 
-
-          Array.Sort(edades);
+            Array.Sort(edades);
             Console.WriteLine("Edad menor es: " + edades[0]);
             Array.Reverse(edades);
             Console.WriteLine("Edad mayor es: " + edades[0]);
