4141b0fc1ee6e4f0bf5c23882a34ea2827c079f7
Anthony Javier Santana Polanco <anthony26uu@gmail.com>
2017-01-13 10:27:24 -0400

    ModifEj4

---
+++ Ej4/Ej4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ej4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, divisor;
+
+
+            Console.Write("\n\n Números primos del 1 al 1000\n\n");
+
+            for (n = 2  ; n <= 1000; n++)
+            {
+                for (divisor = 2; n % divisor != 0; divisor++)
+                {
+                    if (divisor == n)
+                    
+                       Console.Write(" - " );
+
+                     Console.Write(" - "+n);
+                     
+                }
+               
+
+            }
+            Console.ReadKey();
+        }
+    }
+   
+  }
+ 
