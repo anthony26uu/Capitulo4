4f242d47a0f544ac35297ca7ff0f1a34981165ed
Anthony Javier Santana Polanco <anthony26uu@gmail.com>
2017-01-13 01:11:27 -0400

    Modificacion Ejercicio2

---
+++ Ej2/Ej2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double resultado=0;
+            double numero=0;
+            double potencia=0;
+
+            Console.WriteLine("Ingrese Numero a elevar");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese Potencia");
+            potencia = int.Parse(Console.ReadLine());
+
+            resultado = Math.Pow(numero, potencia);
+
+            Console.Write("Resultado " + resultado);
+
+            Console.ReadKey();
+        }
+      
+    }
+}
